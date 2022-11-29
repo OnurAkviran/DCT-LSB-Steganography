75faf562c1ae33a37bff652adc5b3dd778808db6
OnurAkviran <79706505+OnurAkviran@users.noreply.github.com>
2022-11-29 20:05:36 +0300

    Add files via upload

---
+++ lsbSteganography.py
@@ -0,0 +1,96 @@
+import sys
+import numpy as np
+from PIL import Image
+np.set_printoptions(threshold=sys.maxsize)
+
+def encode(src, message, destination):
+    # OPEN AND CREATE AN ARRAY OF THE SOURCE IMAGE
+    sourceImage = Image.open(src,'r')
+    width,height = sourceImage.size
+    array = np.array(list (sourceImage.getdata()))
+
+
+    if sourceImage.mode == 'RGB':
+        n=3
+    elif sourceImage.mode == 'RGBA':
+        n = 4
+    
+    totalPixels = array.size//n
+
+    message += "$END" #STOP FLAG TO SIGNIFY THAT THE MESSAGE IS DONE
+    binaryMessage = ''.join([format(ord(i), "08b") for i in message]) #BINARY CONVERSION OF STRING
+    requiredPixels = len(binaryMessage)
+
+    if requiredPixels > totalPixels:
+        print("Image size not sufficient for the message, try a larger image file")
+        
+    else:
+        i = 0
+        for p in range(totalPixels):
+            for q in range(0,3):
+                if i < requiredPixels:
+                    array[p][q] = int(bin(array[p][q])[2:9] + binaryMessage[i], 2)
+                    i += 1
+        array = array.reshape(height,width,n)
+        encryptedImage = Image.fromarray(array.astype('uint8'), sourceImage.mode)
+        encryptedImage.save(destination)
+        print("Image encoding successful...")
+
+
+def decode(src):
+
+    sourceImage = Image.open(src,'r')
+    array = np.array(list(sourceImage.getdata()))
+
+    if sourceImage.mode == 'RGB':
+        n = 3
+    elif sourceImage.mode == 'RGBA':
+        n = 4
+
+    totalPixels = array.size//n
+
+    hiddenBits = ""
+    for p in range(totalPixels):
+        for q in range(0, 3):
+            hiddenBits += (bin(array[p][q])[2:][-1])
+
+    hiddenBits = [hiddenBits[i:i+8] for i in range(0, len(hiddenBits), 8)]
+
+    message = ""
+    for i in range(len(hiddenBits)):
+        if message[-4:] == "$END":
+            break
+        else:
+            message += chr(int(hiddenBits[i], 2))
+    if "$END" in message:
+        print("Hidden Message:", message[:-4])
+    else:
+        print("No Hidden Message Found")
+
+
+def main():
+    print("LSB STEGANOGRAPHY TOOL")
+    print("Enter 1 to encode an image")
+    print("Enter 2 to decode an image")
+    while True:
+        userInput = input()
+
+        if userInput == "1":
+            print("Enter the source path of the image to encode:")
+            src = input()
+            print("Enter message to be hidden: ")
+            message = input()
+            print("Enter the destination path of your image: ")
+            destination = input()
+
+            encode(src,message,destination)
+
+        elif userInput == "2":
+            print("Enter source path of the image you wish to decode: ")
+            src = input()
+            print("Payload: ")
+            decode(src)
+        else:
+            print("There was an unknown error, try again...")
+
+main()
